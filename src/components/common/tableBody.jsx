c0417c247d07127b32f7300abc7355ca702b5060
Dren-9843 <flnj824@gmail.com>
2021-01-10 15:11:42 -0800

    THIS IS NOT WORING!!!!!!!!!

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -15,13 +15,20 @@ class TableBody extends Component {
         const { data, columns } = this.props;
 
 
-        return (<tbody>
-            {data.map(item => 
-            <tr key ={item._id}>
-            {columns.map(column => <td key={this.createKey(item, column)}>{this.renderCell(item, column)}</td>)}
-            </tr>)}
-        </tbody>  );
-    }
+        return (
+            <tbody>
+                {data.map(item => (
+                    <tr key={item._id}>
+                        {columns.map(column => (
+                        <td key={this.createKey(item, column)}>
+                            {this.renderCell(item, column)}
+                            </td>
+                        ))}
+                    </tr>
+                    ))}
+            </tbody>  
+        );
+}
 }
  
 export default TableBody;
